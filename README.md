f343a1e9174e523394ad298a8ff81424789a439f
unknown <daniel.ruge-g@labinfo.is.escuelaing.edu.co>
2025-09-09 09:24:15 -0500

    punto 3

--- README.md
+++ README.md
@@ -10,3 +10,10 @@
 ### 2 
 
 ![alt text](image-1.png)
+
+### 3
+
+Con @Service, Spring detecta este bean gracias al component-scan,con @Autowired Spring inyecta la dependencia.
+y Con @Qualifier("englishChecker") resolvemos el conflicto entre EnglishSpellChecker y SpanishSpellChecker.
+
+![alt text](image-2.png)
--- taller/src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ taller/src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -3,33 +3,21 @@ package edu.eci.arsw.springdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GrammarChecker {
 
-	SpellChecker sc;
+    @Autowired
+    @Qualifier("englishChecker")  
+    private SpellChecker sc;
 
-	String x;
-        
-        
-	public SpellChecker getSpellChecker() {
-		return sc;
-	}
-
-	public void setSpellChecker(SpellChecker sc) {
-		this.sc = sc;
-	}
-
-
-	public String check(String text){
-		
-		StringBuffer sb=new StringBuffer();
-		sb.append("Spell checking output:"+sc.checkSpell(text));
-		sb.append("Plagiarism checking output: Not available yet");
-		
-		
-		return sb.toString();
-		
-	}
-	
-	
+    public String check(String text){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Spell checking output: ").append(sc.checkSpell(text));
+        sb.append("\nPlagiarism checking output: Not available yet");
+        return sb.toString();
+    }
 }
